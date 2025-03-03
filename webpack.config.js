872ec94488e696e54da812e45b40ac38052a99e2
Timoshka177 <107550627+Timoshka177@users.noreply.github.com>
2025-03-03 11:46:38 +0100

    mise en ligne

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
-
+const path = require('path');
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
@@ -21,7 +21,11 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addAliases({
+        '@symfony/stimulus-bridge/controllers.json': path.resolve(__dirname, 'assets/controllers.json')
+    })
 
+    .enableStimulusBridge('./assets/controllers.json')
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
