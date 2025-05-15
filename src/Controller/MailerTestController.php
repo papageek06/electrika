3f60447092db14423b3c944cfe4cb3f49529ce19
papageek06 <marc.longmar@gmail.com>
2025-05-15 22:38:54 +0200

    debug mail test

--- src/Controller/MailerTestController.php
+++ src/Controller/MailerTestController.php
@@ -1,7 +1,7 @@
 <?php
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,14 +11,21 @@ class MailTestController extends AbstractController
     #[Route('/mail-test', name: 'app_mail_test')]
     public function testMail(MailerInterface $mailer): Response
     {
-        $email = (new Email())
-            ->from('marc.longmar@gmail.com')
-            ->to('tonadresse@gmail.com') // mets ici ton adresse à toi
-            ->subject('Test mail prod')
-            ->text('Ceci est un test d’envoi de mail depuis le serveur de production.');
+        try {
+            $email = (new Email())
+                ->from('marc.longmar@gmail.com')
+                ->to('tonadresse@gmail.com')
+                ->subject('Test SMTP en prod')
+                ->text('Ceci est un test d’envoi de mail depuis le serveur de production.');
 
-        $mailer->send($email);
+            $mailer->send($email);
 
-        return new Response('Test mail envoyé.');
+            return new Response('✅ Mail envoyé avec succès.');
+        } catch (TransportExceptionInterface $e) {
+            return new Response('❌ Erreur d\'envoi de mail : ' . $e->getMessage());
+        } catch (\Throwable $e) {
+            return new Response('❌ Autre erreur : ' . $e->getMessage());
+        }
     }
 }
+
